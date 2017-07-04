f610cf4bae3de30f2ae31f4a267d2f6e03f83ed7
ZHANGYUANHANG <zhangyuanhang@kingsoft.com>
2017-07-04 20:41:01 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,152 @@
 import React, { Component } from 'react';
-import Nav from './Nav';
-import logo from './logo.svg';
+import { Layout } from 'antd';
+import SiderCustom from './components/SiderCustom';
+import HeaderCustom from './components/HeaderCustom';
 import './App.css';
-// 导入Link组件
-import { Link } from 'react-router';
+const { Content, Footer } = Layout;
 
+const menuList = [
+    {
+        icon: 'home',
+        name: '首页',
+        key: 'index',
+        children: [
+        ],
+        path: '/'
+    },
+    {
+        icon: 'message',
+        name: 'HTML',
+        key: 'html',
+        children: [
+            {
+                icon: 'filter',
+                name: '表单',
+                key: 'button',
+                children: [
+                    
+                ],
+                path: '/button'
+            },
+            {
+                icon: 'apple',
+                name: '图标',
+                key: 'button1',
+                children: [
+                    
+                ],
+                path: '/button1'
+            },
+            {
+                icon: 'line-chart',
+                name: '标签页',
+                key: 'button2',
+                children: [
+                    
+                ],
+                path: '/button2'
+            },
+        ],
+        path: '/f'
+    },
+    {
+        icon: 'home',
+        name: 'CSS',
+        key: 'CSS',
+        children: [
+            {
+                icon: 'filter',
+                name: '布局',
+                key: 'layout',
+                children: [
+                    
+                ],
+                path: '/layout'
+            },
+            {
+                icon: 'apple',
+                name: '图标',
+                key: 'icon',
+                children: [
+                    
+                ],
+                path: '/layout'
+            },
+            {
+                icon: 'line-chart',
+                name: 'CSS3',
+                key: 'CSS3',
+                children: [
+                    
+                ],
+                path: '/css3'
+            },
+        ],
+        path: '/CSS'
+    },
+    {
+        icon: 'home',
+        name: 'JavaScript',
+        key: 'JavaScript',
+        children: [
+
+        ],
+        path: '/JavaScript'
+    },
+];
 class App extends React.Component {
+ 	state = {
+        collapsed: false,
+    };
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+	// getInitialState 初始化组件状态
+	// constructor(props) {
+	// 	super(props);
+	// 	console.log("getInitialState");
+	// 	this.state = {
+
+	// 	}
+	// }
+	// 组件第一次加载时渲染完成的事件，一般在此获取网络数据。实际开始项目开发时，会经常用到。
+	comopentDidMount() {
+		// console.log("comopentDidMount");
+	}
+
+	// 主要用于性能优化，React 的性能优化也是一个很重要的话题，后面一并讲解
+	shouldComponentUpdate(nextProps, nextState) {
+		// console.log("shouldComponentUpdate");
+		return true;
+	}
+
+	// 组件更新了之后触发的事件，一般用于清空并更新数据。实际开始项目开发时，会经常用到。
+	componentDidUpdate(prevProps, prevState) {
+		// console.log("componentDidUpdate");
+	}
+
+	// 组件在销毁之前触发的事件，一般用户存储一些特殊信息，以及清理setTimeout事件等。
+	componentWillUnmount() {
+		// console.log("componentWillUnmount");
+	}
+
+	// render 返回组件要渲染的模板
 	render() {
 		return (
-			<div className="App">
-				<Nav>
-				</Nav>
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Welcome to React</h2>
-				</div>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-				<h1>React Router Tutorial</h1>
-	            <ul role="nav">
-	                <li><Link to="/productList">About</Link></li>
-	                <li><Link to="/repos">Repos</Link></li>
-	            </ul>
-	            <div className="productList">
-					{this.props.children}
-	            </div>
-			</div>
+			<Layout className="ant-layout-has-sider" style={{ height: '100%' }}>
+				<SiderCustom path={this.props.location.pathname} userName={ '张远航' } menuList={ menuList } collapsed={this.state.collapsed} toggle={this.toggle} />
+				<Layout>
+	                <HeaderCustom collapsed={this.state.collapsed} toggle={this.toggle} />
+	                <Content style={{ margin: '0 16px', overflow: 'initial' }}>
+	                  	{this.props.children}
+	                </Content>
+	                <Footer style={{ textAlign: 'center' }}>
+	                  	React-Admin ©2017 Created by 865470087@qq.com
+	                </Footer>
+              	</Layout>
+			</Layout>
 		);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRedirect,IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRedirect, IndexRoute, browserHistory, Redirect,Lifecycle, RouteContext } from 'react-router';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-import Menu from './common/menu/Menu';
+// import Menu from './common/menu/Menu';
 import ProductList from './page/ProductList';
-import {DatePicker,Button} from 'antd';
-import 'antd/dist/antd.css';
+// import {DatePicker,Button} from 'antd';
 import Index from './components/Index';
+import Login from './components/Login/Login';
 
-// const routes = 
-// 	<Route path={'/'} component={App} />
-// 	<Route path={'/productList'} component={ProductList} />;
+import './index.css';
+import 'antd/dist/antd.css';
+
+function checkPermission(nextState, replace, next) {
+	// console.log(window.localStorage.userId);
+	if ( window.localStorage.userId ) {
+		next();
+	} else if ( nextState.location.pathname !== '/login' ) {
+		replace('/login');
+		next();
+	} else {
+		next();
+	}
+}
+
+const routes = 
+	<Route onEnter={ checkPermission }>
+		<Route path={ '/' } component={ App } >
+	    	<IndexRoute component={ Index } />
+			<Route path={ '/productList/:product' } component={ ProductList } />
+			
+			<Redirect from="messages/:id" to="/messages/:id" />
+		</Route>
+		<Route path={ '/login' } component={ Login } />
+	</Route>;
 
 
 ReactDOM.render(
 	<Router history={browserHistory}>
-        <Route path={'/'} component={App}>
-        	<IndexRoute component={Index} />
-			<Route path={'/productList/:product'} component={ProductList} />
-		</Route>
+        {routes}
     </Router>, 
     document.getElementById('app')
 );
-// ReactDOM.render(<Nav />, document.getElementById('nav'));
-// ReactDOM.render(<Menu />, document.getElementById('menu'));
-// ReactDOM.render(<ProductList />, document.getElementById('productList'));
-// ReactDOM.render(<DatePicker />, document.getElementById('DatePicker'));
 registerServiceWorker();
